75a322a7394412fec3ed5e69269ffdd111195d19
Rosângela Nery <rosangela2nery@gmail.com>
2022-09-20 17:30:16 -0300

    Creating database connection

--- index.js
+++ index.js
@@ -11,4 +11,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+
+let db;
+mongoClient.connect().then(() => {
+    db = mongoClient.db("drivencracy");
+});
+
 app.listen(5000, () => console.log("App rummin in port 5000"));
