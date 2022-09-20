92f027b830380af7cb318cba8eb46df4d5bceb12
Rosângela Nery <rosangela2nery@gmail.com>
2022-09-20 17:51:38 -0300

    Creating validations with joi

--- index.js
+++ index.js
@@ -11,11 +11,28 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Conexão com o meu banco de dados:
 const mongoClient = new MongoClient(process.env.MONGO_URI);
 
 let db;
 mongoClient.connect().then(() => {
     db = mongoClient.db("drivencracy");
+});
+
+// Validação de dados com o joi:
+const surveySchema = joi.object({
+    title: joi.string().min(3).required(),
+    expireAt: joi.number().required()
+});
+
+const votingOptionsSchema = joi.object({
+    title: joi.string().min(1).required(),
+    pollId: joi.number().required()
+});
+
+const formatOfAVoteSchema = joi.object({
+    createdAt: joi.number().required(),
+    choiceID: joi.number().required()
 });
 
 app.listen(5000, () => console.log("App rummin in port 5000"));
